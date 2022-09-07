1ad8fa5ffbc718ab7432e57e6cd6f0f04eb8d92e
Franpan94 <francescopanico94@libero.it>
2022-09-07 17:01:49 +0200

    import img

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,3 +1,7 @@
-<header>
-    Inizio
-</header>
+<header class="d-flex justify-around pb-2">
+    <img src="../../../public/assets/img/dc-logo-bg.png" alt="dc-logo" class="width" />
+
+    <ul class="d-flex text">
+      <a href="">voce</a>
+    </ul>
+  </header>
